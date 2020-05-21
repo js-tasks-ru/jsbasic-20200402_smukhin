d4886a555a18ba7d406ecc681a70da10212d40e2
sashaskywalker13 <53815710+sashaskywalker13@users.noreply.github.com>
2020-05-21 15:39:26 +0300

    8-module 3-task

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,60 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    let cartItem = {
+      product: product,
+      count: 1,
+    };
+
+    let cart = this.cartItems.find((item) => item.product.name === product.name);
+    if (cart === undefined) {
+      this.cartItems.push(cartItem);
+    }
+
+    if (cart !== undefined) {
+      cart.count = cart.count + 1;
+      cart.product.price += cart.product.price;
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.cartItems.find((item) => item.product.id === productId);
+    let priceCartItem = cartItem.product.price / cartItem.count;
+
+    if (amount === -1) {
+      cartItem.count = cartItem.count - 1;
+      cartItem.product.price -= priceCartItem;
+    }
+    if (amount === 1) {
+      cartItem.count = cartItem.count + 1;
+      cartItem.product.price += priceCartItem;
+    }
+    if (cartItem.count === 0) {
+      this.cartItems.splice(cartItem);
+    }
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    if (this.cartItems.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getTotalCount() {
-    // ваш код
+    let total = 0;
+    this.cartItems.map((item) => total += item.count);
+    return total;
   }
 
   getTotalPrice() {
-    // ваш код
+    let total = 0;
+    this.cartItems.map((item) => total += item.product.price);
+    return total;
   }
 
   onProductUpdate() {
